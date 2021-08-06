e5b9091cae951dc219165b578328c3bbdfb44ef7
suchitrathapa <83858230+suchitrathapa@users.noreply.github.com>
2021-08-06 10:26:06 +0545

    Adding my first task script

---
+++ Script_HackBiotask1.R
@@ -0,0 +1,23 @@
+#Script to display my personal details using R language
+
+#Create variables
+Name<-'Suchitra Thapa'
+Email<-'suchitrathapa69@gmail.com'
+Slack_username<-'@Suchitraa'
+Biostack<-'Public Health and Genomic Epidemiology'
+Twitter_handle<-'@Cheetrasu'
+
+#Create vectors of the string variables
+x<- c("@","S","u","c","h","i","t","r","a","a")
+y<- c("@","C","h","e","e","t","r","a","s","u")
+
+#Find the hamming distance between vectors
+sum(x!=y)
+Hamming_Distance<- sum(x!=y)
+
+#Join the variables to create a dataframe
+df<-data.frame(Name,Email,Slack_username,Biostack,Twitter_handle,Hamming_Distance)
+
+#Visualize the dataframe
+df
+View(df)
