5c305e23d73d744a00f3747fbc9ba2770f573e1e
RitojaR <88329693+RitojaR@users.noreply.github.com>
2021-08-06 22:39:45 +0530

    Update Hookebash.sh

--- Hookebash.sh
+++ Hookebash.sh
@@ -7,7 +7,7 @@ git clone https://github.com/RitojaR/TeamHooke
 Chmod +x TeamHooke
 
 #Change the directory to TeamHooke from current
-cd TeamHooke
+cd ./TeamHooke
 
 #create the header
 echo Name, Email, Slack_username, Biostack, Twitter_handle, Hamming_distance > teamCSV.csv
