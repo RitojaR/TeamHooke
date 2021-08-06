fc79da431494cfde0ae167bd8b3bfd829c3976a2
RitojaR <88329693+RitojaR@users.noreply.github.com>
2021-08-06 22:34:52 +0530

    Update RitojaTask0.R

--- RitojaTask0.R
+++ RitojaTask0.R
@@ -1,4 +1,3 @@
-# Rscript 
 
 name<-"Ritoja Ray"
 email<-"ritojaray@hotmail.com"
@@ -11,8 +10,9 @@ for (i in 1:nchar(twitter)){ #as twitter username is longer
 check1<-stri_sub(slack,i,i)
 check2<-stri_sub(twitter,i,i)
 if(check1 != check2){
-count<-count+4
-}
-}
-output<-c(name,email,slack,biostack,twitter,count)
-output
+count<-count+1
+}#end of if
+}#end of for
+output<-data.frame(name,email,slack,biostack,twitter,count)
+cat(paste(output,collapse=','))
+#end
