b7803b2744b04897d845cb4162efe88654e88726
suchitrathapa <83858230+suchitrathapa@users.noreply.github.com>
2021-08-06 15:39:33 +0545

    Update Script_HackBiotask1.R

--- Script_HackBiotask1.R
+++ Script_HackBiotask1.R
@@ -7,13 +7,15 @@ Slack_username<-'@Suchitraa'
 Biostack<-'Public Health and Genomic Epidemiology'
 Twitter_handle<-'@Cheetrasu'
 
-#Create vectors of the string variables
-x<- c("@","S","u","c","h","i","t","r","a","a")
-y<- c("@","C","h","e","e","t","r","a","s","u")
-
-#Find the hamming distance between vectors
-sum(x!=y)
-Hamming_Distance<- sum(x!=y)
+#Create variables 
+x <- '@Suchitraa'
+y <- '@Cheetrasu'
+#Split each of the string variables
+A <- strsplit(x, split = "")[[1]]
+B <- do.call("cbind", strsplit(y, split = ""))
+#Combine the column created
+D <- colSums(A != B)
+Hamming_Distance<- min(D)
 
 #Join the variables to create a dataframe
 df<-data.frame(Name,Email,Slack_username,Biostack,Twitter_handle,Hamming_Distance)
