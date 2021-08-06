54b4a42c5f1e1ddd34e4cd985e35278dbb008054
RitojaR <88329693+RitojaR@users.noreply.github.com>
2021-08-06 19:43:32 +0530

    test commit by Ritoja

--- RitojaTask0.R
+++ RitojaTask0.R
@@ -11,7 +11,7 @@ for (i in 1:nchar(twitter)){ #as twitter username is longer
 check1<-stri_sub(slack,i,i)
 check2<-stri_sub(twitter,i,i)
 if(check1 != check2){
-count<-count+1
+count<-count+4
 }
 }
 output<-c(name,email,slack,biostack,twitter,count)
