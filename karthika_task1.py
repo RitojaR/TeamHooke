8cd97b61c86f930d1eaecd866cb0ba7ad6f0cc44
Karthika <71928132+karthikavarmar@users.noreply.github.com>
2021-08-06 21:06:01 +0530

    Update karthika_task1.py

--- karthika_task1.py
+++ karthika_task1.py
@@ -1,11 +1,15 @@
-#This will print the details about me as instructed by the admins
-keys = ["Name:", "E-Mail ID:", "SlackID:", "Biostack:", "TwitterID:"]
-values = ["Karthika R", "karthikavarmar@gmail.com", "@karthika", "Drug Discovery","@_kvarma"]
-print ("\n".join("{} {}".format(k,v) for k,v in zip(keys, values)))
 
-#This will print the hamming distance between the slack and twitter IDs
+name = "Karthika R"
+email_id = "karthikavarmar@gmail.com"
+slack_id = "@karthika"
+biostack = "Drug Development"
+twitter_id = "@_kvarma"
+
+#This is to find the hamming distance between the slack and twitter IDs
 def hamming_distance(s1, s2):
-    return sum(c1 != c2 for c1, c2 in zip(s1, s2))
-s1 = values[2]
-s2 = values[4]
-print("\nHamming Distance: ", hamming_distance(s1,s2))
+    return sum(c1 != c2 for c1, c2 in zip(slack_id, twitter_id))
+
+#This will print the details about me as instructed by the admins
+print(name,",",email_id,",",slack_id,",",biostack,",",twitter_id,",",hamming_distance(slack_id, twitter_id))
+
+
