09d05ea53de44ec2530a9e4eec0d1e63354559b8
Diya Arul <67481283+diyaarul2981@users.noreply.github.com>
2021-08-07 12:45:46 +0530

    Update Jaykishan_Ruby.rb

--- Jaykishan_Ruby.rb
+++ Jaykishan_Ruby.rb
@@ -1,7 +1,1 @@
-puts "Name:            Solanki Jaykishan";
-puts "Email:           solankijaykishan91@gmail.com";
-puts "Slack_Username:  @Jay";
-puts "Biostack:        Drug_Development";
-puts "Twitter:         @Ja9"
-puts "hamming distance between Slack and Twitter is:";
-puts "@Jay" <=> "@Ja9";
+puts "Solanki Jaykishan, solankijaykishan91@gmail.com, @Jay, Drug_Development, @Ja9,#{"@Jay" <=> "@Ja9"}";
