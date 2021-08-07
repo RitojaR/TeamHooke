802c8c83d9eba9cdb86a0e3637564cc98e0d4dd6
Radhu Ladani <70321942+Radhu903@users.noreply.github.com>
2021-08-07 03:35:42 -0500

    Add files via upload
    kindly fine attachment of c file

---
+++ Radhu_C.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int i, length, count = 0;
+    char v1[8], v2[8];
+    printf("\n Radhu Ladani");
+    printf("\n radhuladani62@gmail.com");
+    printf("\n @Radhu");
+    printf("\n Genomics");
+    printf("\nWrite a Program to Find Hamming Distance\n");
+    printf("Enter Slack id: ");
+    scanf("%s", &v1);
+    printf("Enter Twitter id: ");
+    scanf("%s", &v2);
+
+    length = strlen(v2);
+    for(i = 0; i < length; i++)
+    {
+        if(v1[i] != v2[i])
+        {
+            count++;
+        }
+    }
+
+    printf("\nHamming Distance : %d", count);
+
+}
