f94057367652d149fbdaa702c337e13513eaa35a
Jaykishan <86801284+JaykishanJ@users.noreply.github.com>
2021-08-06 23:29:28 -0500

    Update Jaykishan_Ruby.rb

--- Jaykishan_Ruby.rb
+++ Jaykishan_Ruby.rb
@@ -2,6 +2,6 @@ puts "Name:            Solanki Jaykishan";
 puts "Email:           solankijaykishan91@gmail.com";
 puts "Slack_Username:  @Jay";
 puts "Biostack:        Drug_Development";
-puts "Twitter:         @Jayk"
+puts "Twitter:         @Ja9"
 puts "hamming distance between Slack and Twitter is:";
-puts "@Jay" <=> "@Jayk";
+puts "@Jay" <=> "@Ja9";
